5abc85a0e494ee04561c978d4989a7ea51480468
JaeHwan ma <kohkss@naver.com>
2020-01-07 14:08:19 +0900

    짝대기 추가

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -21,105 +21,103 @@ public class HomeController extends HttpServlet {
 		Forward fw=null;
 		
 		switch (cmd) {
-		case "main": //메인화면
+		case "/main": //메인화면
+			Login lg=new Login(request,response);
+			fw=lg.check();
 			break;
 		
-		
-			
-			
-			
-		case "signup": //회원가입
+		case "/signup": //회원가입
 			break;
 
 			
 			
 			
 			
-		case "login": //로그인
+		case "/login": //로그인
 			break;
 			
 			
 			
 			
 			
-		case "idsearch": //아이디 찾기
+		case "/idsearch": //아이디 찾기
 			break;
 			
 			
 			
 			
 		
-		case "pwsearch": //비밀번호 찾기
+		case "/pwsearch": //비밀번호 찾기
 			break;
 			
 			
 			
 			
-		case "dropmember": //회원탈퇴
+		case "/dropmember": //회원탈퇴
 			break;
 			
 			
 			
 			
-		case "searchboard": //통합검색
+		case "/searchboard": //통합검색
 			break;
 			
 			
 			
-		case "freewebfiction": //무료 웹 소설
-			break;
-			
-			
-			
-			
-		case "faidewebfiction": //유료 웹 소설
-			break;
-			
-			
-			
-		case "noveldetail": //작품 상세 페이지
-			break;
-			
-			
-			
-		case "buynovel": //작품 구매 페이지, 라이트박스 이용
-			break;
-			
-			
-			
-		case "viewer": //작품 보기
-			break;
-			
-			
-			
-		case "report": //신고하기
-			break;
-			
-			
-			
-		case "write": //글쓰기
-			break;
-			
-			
-			
-		case "bestwebnovel": //베스트 소설 목록
+		case "/freewebfiction": //무료 웹 소설
 			break;
 			
 			
 			
 			
-		case "myPage": //마이페이지 
+		case "/faidewebfiction": //유료 웹 소설
+			break;
+			
+			
+			
+		case "/noveldetail": //작품 상세 페이지
+			break;
+			
+			
+			
+		case "/buynovel": //작품 구매 페이지, 라이트박스 이용
+			break;
+			
+			
+			
+		case "/viewer": //작품 보기
+			break;
+			
+			
+			
+		case "/report": //신고하기
+			break;
+			
+			
+			
+		case "/write": //글쓰기
+			break;
+			
+			
+			
+		case "/bestwebnovel": //베스트 소설 목록
 			break;
 			
 			
 			
 			
-		case "payment": //결제하기
+		case "/myPage": //마이페이지 
+			break;
+			
+			
+			
+			
+		case "/payment": //결제하기
 			break;
 			
 			
 		
-		case "authorchange": //유료작가로 전환 신청
+		case "/authorchange": //유료작가로 전환 신청
 			break;
 			
 			
